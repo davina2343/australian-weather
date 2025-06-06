376bea627f887ef4c299ce8522b298a15550ddde
davina2343 <davina.gidda@tokiomarinekiln.com>
2025-06-06 12:03:24 +0100

    theme classic

--- R/create-plot.R
+++ R/create-plot.R
@@ -1,7 +1,8 @@
 # Simple script analysing the Australian weather data
+library("usethis")
+use_git_config(user.name = "davina2343",
+               user.email = "davina.gidda@tokiomarinekiln.com")
 
-git config --global user.email "davina.gidda@tokiomarinekiln.com"
-git config --global user.name "davina2343"
 
 library("tidyverse")
 
@@ -19,6 +20,6 @@ df %>%
   filter(Location == 'Sydney') %>%
   ggplot(aes(x = Temp9am, y = Humidity9am) ) + 
   geom_point( colour ='red')
-
+ 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
